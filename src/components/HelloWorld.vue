8edc7e5de7d278f0840faaf4e983df97b7f3b839
武为江 <by_wwj@163.com>
2018-04-24 09:31:42 +0800

    更改了样式。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -52,57 +52,7 @@
         </Menu>
       </Header>
       <router-view></router-view>
-      <!--<Layout :style="{padding: '0 50px'}">
-        <Breadcrumb :style="{margin: '16px 0'}">
-          <BreadcrumbItem>Home</BreadcrumbItem>
-          <BreadcrumbItem>Components</BreadcrumbItem>
-          <BreadcrumbItem>Layout</BreadcrumbItem>
-        </Breadcrumb>
-        <Content :style="{background: '#fff',overflow:'auto'}">
-          <Layout>
-            <Sider hide-trigger :style="{background: '#fff'}">
-              <Menu active-name="2-2" theme="light" width="auto" :open-names="['2']" accordion @on-select="select">
-                <Submenu name="1">
-                  <template slot="title">
-                    <Icon type="ios-navigate"></Icon>
-                    Item 1
-                  </template>
-                  <MenuItem name="option1" >Option 1</MenuItem>
-                  <MenuItem name="calTender" >计算标底</MenuItem>
-                  <MenuItem name="1-3">Option 3</MenuItem>
-                </Submenu>
-                <Submenu name="2">
-                  <template slot="title">
-                    <Icon type="ios-keypad"></Icon>
-                    Item 2
-                  </template>
-                  <MenuItem name="2-1">Option 1</MenuItem>
-                  <MenuItem name="2-2">Option 2</MenuItem>
-                </Submenu>
-                <Submenu name="3">
-                  <template slot="title">
-                    <Icon type="ios-analytics"></Icon>
-                    Item 3
-                  </template>
-                  <MenuItem name="3-1">Option 1</MenuItem>
-                  <MenuItem name="3-2">Option 2</MenuItem>
-                </Submenu>
-                <Submenu name="4">
-                  <template slot="title">
-                    <Icon type="ios-analytics"></Icon>
-                    Item 4
-                  </template>
-                  <MenuItem name="4-1">Option 1</MenuItem>
-                  <MenuItem name="4-2">Option 2</MenuItem>
-                </Submenu>
-              </Menu>
-            </Sider>
-            <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-              <router-view></router-view>
-            </Content>
-          </Layout>
-        </Content>
-      </Layout>-->
+      
       <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
     </Layout>
   </div>
